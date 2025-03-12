7862ef8b23915099da06aff0a431eb22e155fcfc
c1309047@st.kanazawa-it.ac.jp <c1309047@st.kanazawa-it.ac.jp>
2025-03-12 17:24:12 +0900

    バックエンドを少しだけ修正

--- app.py
+++ app.py
@@ -1,22 +1,22 @@
-from flask import Flask,render_template,request
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
+
 @app.route('/')
 def hello_world():
     return "Hello world"
 
 @app.route("/cal2", methods=['GET', 'POST'])
 def cal2():
-    item.id, item.name, item.category, item.price, item.stock = 
-    "0","0","0","0",
-
-
-    return render_template('', item.id, item.name, item.category, item.price, item.stock)
+    item = {
+        "id": "0",
+        "name": "0",
+        "category": "0",
+        "price": "0",
+        "stock": "0"
+    }
+    return render_template('cal2.html', item=item)
 
 if __name__ == '__main__':
-<<<<<<< HEAD
-    app.debug = True
+    app.debug = True  
     app.run(host='localhost', port=8000)
-=======
-    app.run(host='0.0.0.0', port=3000)
->>>>>>> a5bbb4c816edc0a25a0d87a2237405fd21cdd13b
