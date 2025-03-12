a5bbb4c816edc0a25a0d87a2237405fd21cdd13b
I2693 <c1302693@st.kanazawa-it.ac.jp>
2025-03-12 16:35:24 +0900

    Add files via upload

--- Team_AI/hacka.py
+++ Team_AI/hacka.py
@@ -16,8 +16,10 @@ hidden_units = 32
 
 X = subset[0][:12].reshape(1, sequence_length, 1)
 y = subset[0][1:13].reshape(1, sequence_length, 1)
-x=X/sum(X)
-y=y/sum(y)
+sum_x=np.sum(X)
+sum_y=np.sum(y)
+X=X/sum_x
+y=y/sum_y
 
 model = Sequential()
 model.add(LSTM(hidden_units, input_shape=(sequence_length, 1)))
@@ -27,3 +29,7 @@ model.add(Dense(1))
 model.compile(optimizer=Adam(learning_rate=0.001), loss='mse')
 
 model.fit(X, y, epochs=200, batch_size=1)
+
+prediction=model.predict(subset[0][12].reshape(1, 1, 1))
+
+print(prediction*sum_x)
