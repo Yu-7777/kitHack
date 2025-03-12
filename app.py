4d3d05ecdbbe769faf8713ab243563b5f0447253
c1309047@st.kanazawa-it.ac.jp <c1309047@st.kanazawa-it.ac.jp>
2025-03-12 21:35:50 +0900

    appをデータベースに合わせて修正

--- app.py
+++ app.py
@@ -1,36 +1,49 @@
 from flask import Flask, render_template, request
 import sqlite3
+import datetime
 from setup.createtable  import createTable
+
 
 
 
 dt_now = datetime.datetime.now()
 app = Flask(__name__)
 
+def get_inventory():
+    con = sqlite3.connect("stock.db")
+    cur = con.cursor()
+    cur.execute("SELECT * FROM zaiko")
+    rows = cur.fetchall()
+    con.close()
+    return rows
+
+
 @app.route('/')
 def hello_wosrld():
-
     return "Hello world"
 
-@app.route('/test')
-def test():
-    con = sqlite3.connect("test.db")
-    cur = con.cursor()
-    dblen, dblist = 0, []
-    
-    if request.methods="GET":
-        var1 = None
-    else:
-        name = request.form.get('name','')
-        num = request.form.get('num','')
-        input = request.form.get('input','')
-        output = request.form.get('output','')
-        category = request.form.get('category','')
+@app.route('/test', methods=['GET','POST'])
+def test(): 
+    if request.methods="POST":
+        
+        name = request.form.get('name', '')
+        num = request.form.get('num', '')
+        input_date = request.form.get('input', '')
+        output_date = request.form.get('output', '')
+        cold = request.form.get('cold', '')
+        price = request.form.get('price', '')
+        category = request.form.get('category', '')
 
+        con = sqlite3.connect("stock.db")
+        cur = con.cursor()
+        cur.execute("""
+            INSERT INTO zaiko (name, input, output, cold, price, category)
+            VALUES (?, ?, ?, ?, ?, ?)
+        """, (name, input_date, output_date, cold, price, category))
         con.commit()
-        cur.execute("SELECT * FROM log ORDER BY id DESC")
-        dblist = cur.fetchall()
         con.close()
+
+    dblist = get_inventory()
 
     return render_template('test.html', dblen=len(dblist), dblist = dblist)
 
